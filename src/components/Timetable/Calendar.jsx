10190adec6bf6cb5f35b785bccf63448b2f7d2c3
risquaMussa <rismussa@gmail.com>
2025-04-12 16:42:51 -0400

    Fix Calendar layout and ensure location displays

--- src/components/Timetable/Calendar.jsx
+++ src/components/Timetable/Calendar.jsx
@@ -21,6 +21,9 @@ const Calendar = () => {
           <tr>
             <td className="time">8 am</td>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td>
               <Event
                 event="Starbucks ☕"
@@ -28,6 +31,7 @@ const Calendar = () => {
                 color="green"
               />
             </td>
+<<<<<<< HEAD
             <td></td>
             <td></td>
             <td>
@@ -83,37 +87,40 @@ const Calendar = () => {
               location="646 Michigan Ave"
               color="green"
             />
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td></td>
             <td></td>
+            <td>
+              <Event event="Yolk 🍳" location="355 E Ohio St" color="green" />
+            </td>
             <td></td>
-            <td></td>
-            <Event event="Yolk 🍳" location="355 E Ohio St" color="green" />
-            <td></td>
+            <td>
+              <Event event="Yolk 🍳" location="355 E Ohio St" color="green" />
+            </td>
+            <td>
+              <Event event="Subway 🚇" location="Grand Station" color="pink" />
+            </td>
           </tr>
+
           <tr>
             <td className="time">9 am</td>
+            <td>
+              <Event event="Subway 🚇" location="Grand Station" color="pink" />
+            </td>
             <td></td>
+            <td>
+              <Event event="Subway 🚇" location="Grand Station" color="pink" />
+            </td>
             <td></td>
+            <td>
+              <Event
+                event="River Cruise ⛵"
+                location="Chicago River"
+                color="blue"
+              />
+            </td>
             <td></td>
-            <Event event="Subway 🚇" location="Grand Station" color="pink" />
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td className="time">10 am</td>
-            <td></td>
-            <Event
-              event="River Cruise ⛵"
-              location="Chicago River"
-              color="blue"
-            />
->>>>>>> 14d751f (Timetable)
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-<<<<<<< HEAD
             <td>
               <Event
                 event="The Bean 🫘"
@@ -123,15 +130,58 @@ const Calendar = () => {
             </td>
           </tr>
 
+          <tr>
+            <td className="time">10 am</td>
+<<<<<<< HEAD
+            <td></td>
+            <Event
+              event="River Cruise ⛵"
+              location="Chicago River"
+              color="blue"
+            />
+>>>>>>> 14d751f (Timetable)
+=======
+            <td>
+              <Event event="Deep Dish 🍕" location="Giordano’s" color="green" />
+            </td>
+            <td>
+              <Event event="Deep Dish 🍕" location="Giordano’s" color="green" />
+            </td>
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
+            <td>
+              <Event
+                event="The Bean 🫘"
+                location="Millennium Park"
+                color="blue"
+              />
+            </td>
+<<<<<<< HEAD
+          </tr>
+
 =======
             <Event event="The Bean 🫘" location="Millennium Park" color="blue" />
           </tr>
 >>>>>>> 14d751f (Timetable)
+=======
+          </tr>
+
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
           <tr>
             <td className="time">11 am</td>
             <td></td>
             <td></td>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td>
               <Event event="Deep Dish 🍕" location="Giordano’s" color="green" />
             </td>
@@ -142,18 +192,25 @@ const Calendar = () => {
                 color="pink"
               />
             </td>
+<<<<<<< HEAD
 =======
             <Event event="Deep Dish 🍕" location="Giordano’s" color="green" />
             <td></td>
 >>>>>>> 14d751f (Timetable)
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td></td>
             <td></td>
             <td></td>
           </tr>
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 14d751f (Timetable)
+=======
+
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
           <tr>
             <td className="time">12 pm</td>
             <td></td>
@@ -161,6 +218,9 @@ const Calendar = () => {
             <td></td>
             <td></td>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td>
               <Event
                 event="Subway 🚇"
@@ -168,6 +228,7 @@ const Calendar = () => {
                 color="pink"
               />
             </td>
+<<<<<<< HEAD
             <td></td>
             <td>
               <Event
@@ -255,71 +316,98 @@ const Calendar = () => {
           </tr>
 
 =======
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td></td>
-            <Event
-              event="Subway 🚇"
-              location="Washington Station"
-              color="pink"
-            />
-            <td></td>
+            <td>
+              <Event
+                event="Art Institute 🖼️"
+                location="111 S Michigan Ave"
+                color="blue"
+              />
+            </td>
           </tr>
+
           <tr>
             <td className="time">1 pm</td>
+            <td>
+              <Event
+                event="Girl & the Goat 🍽️"
+                location="809 W Randolph St"
+                color="green"
+              />
+            </td>
             <td></td>
+            <td>
+              <Event
+                event="Art Institute 🖼️"
+                location="111 S Michigan Ave"
+                color="blue"
+              />
+            </td>
             <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>
+              <Event
+                event="Subway 🚇"
+                location="Roosevelt Station"
+                color="pink"
+              />
+            </td>
+            <td>
+              <Event
+                event="Girl & the Goat 🍽️"
+                location="809 W Randolph St"
+                color="green"
+              />
+            </td>
+            <td>
+              <Event
+                event="Fancy Dinner 🎩"
+                location="Maple & Ash"
+                color="green"
+              />
+            </td>
           </tr>
-          <tr>
-            <td className="time">2 pm</td>
-            <td></td>
-            <Event
-              event="Art Institute 🖼️"
-              location="111 S Michigan Ave"
-              color="blue"
-            />
-            <td></td>
-            <td></td>
-            <Event
-              event="Girl & the Goat 🍽️"
-              location="809 W Randolph St"
-              color="green"
-            />
-            <td></td>
-            <td></td>
-          </tr>
+
           <tr>
             <td className="time">3 pm</td>
-            <Event event="Cubs Game ⚾" location="Wrigley Field" color="blue" />
+            <td>
+              <Event
+                event="Cubs Game ⚾"
+                location="Wrigley Field"
+                color="blue"
+              />
+            </td>
+            <td>
+              <Event
+                event="Cubs Game ⚾"
+                location="Wrigley Field"
+                color="blue"
+              />
+            </td>
             <td></td>
             <td></td>
             <td></td>
-            <td></td>
-            <Event
-              event="Subway 🚇"
-              location="Roosevelt Station"
-              color="pink"
-            />
-            <td></td>
+            <td>
+              <Event
+                event="Subway 🚇"
+                location="Roosevelt Station"
+                color="pink"
+              />
+            </td>
+            <td>
+              <Event
+                event="Shopping 🛍️"
+                location="Magnificent Mile"
+                color="blue"
+              />
+            </td>
           </tr>
-          <tr>
-            <td className="time">4 pm</td>
-            <td></td>
-            <Event
-              event="Fancy Dinner 🎩"
-              location="Maple & Ash"
-              color="green"
-            />
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+<<<<<<< HEAD
 >>>>>>> 14d751f (Timetable)
+=======
+
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
           <tr>
             <td className="time">5 pm</td>
             <td></td>
@@ -328,6 +416,9 @@ const Calendar = () => {
             <td></td>
             <td></td>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
             <td>
               <Event
                 event="Shopping 🛍️"
@@ -335,6 +426,7 @@ const Calendar = () => {
                 color="blue"
               />
             </td>
+<<<<<<< HEAD
             <td></td>
 =======
             <td></td>
@@ -344,6 +436,9 @@ const Calendar = () => {
               color="blue"
             />
 >>>>>>> 14d751f (Timetable)
+=======
+            <td></td>
+>>>>>>> e848d1d (Fix Calendar layout and ensure location displays)
           </tr>
         </tbody>
       </table>
